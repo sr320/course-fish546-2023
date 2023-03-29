9a1fe04ab34b29deec3de452d0dc86c95298fc43
Steven Roberts <sr320@uw.edu>
2023-03-29 12:23:53 -0700

    go works

--- shiny/app.R
+++ shiny/app.R
@@ -1,62 +1,74 @@
 library(shiny)
 library(dplyr)
 
-# Define UI
+
+
+# Define the UI
 ui <- fluidPage(
-  headerPanel("My First Shiny App"),
-  sidebarLayout(
-    sidebarPanel(
-      # Add input field for the user to provide a value for the column
-      textInput("column_value", "Enter value for column:"),
-      actionButton("submit", "Submit")
-    ),
-    mainPanel(
-      p("Enter the evalue you want to query."),
-      # Add table or other visualizations to display the subset of the table
-      tableOutput("table")
-    )
-  )
+  headerPanel("My second Shiny App"),
+  # Input for the search term
+  textInput("search_term", "GO Biological Process Search term:"),
+  
+  # Table to display the search results
+  tableOutput("search_results")
 )
 
-# Define server
+# Define the server
 server <- function(input, output) {
-  # Read CSV file from GitHub repository
+  # Load the data
   data <- read.csv("https://raw.githubusercontent.com/sr320/course-fish546-2023/main/output/blast_annot_go.tab", sep = '\t', header = TRUE)
-  
-  # Define reactive expression to subset table based on user input
- # subset_data <- reactive({
- #   subset(data, V13 == input$column_value) # Replace COLUMN_NAME with the name of the column you want to filter on
- # })
-  
-  # Assume you have a data frame called "df" with a column called "col"
-  # where you want to search for partial matches
-  
-  partial_match <- function(text, pattern) {
-    grepl(pattern, text, ignore.case = TRUE)
-  }
-  
-  # Define a function to filter the data frame
-  filter_df <- function(pattern) {
-    data[apply(data, 1, partial_match, pattern), ]
-  }
-  
-  # In your Shiny app, create an input text box for the user to input the search term
-  textInput("search_term", "Search term:")
-  
-  # Use an observeEvent to filter the data frame whenever the user inputs a search term
-  observeEvent(input$search_term, {
-    filtered_df <- filter_df(input$search_term)
-    # Use the filtered_df to display the results in your Shiny app
+
+  # Define a reactive expression to filter the data based on the search term
+  filtered_data <- reactive({
+    subset(data, grepl(input$search_term, Gene.Ontology..biological.process., ignore.case = TRUE))
   })
   
-  
-  
-  
-  # Render table based on subset_data reactive expression
-  output$table <- renderTable({
-    filtered_df()
+  # Display the search results in a table
+  output$search_results <- renderTable({
+    filtered_data()
   })
 }
 
 # Run the app
 shinyApp(ui, server)
+
+
+
+# 
+# # Define UI
+# ui <- fluidPage(
+#   headerPanel("My First Shiny App"),
+#   sidebarLayout(
+#     sidebarPanel(
+#       # Add input field for the user to provide a value for the column
+#       textInput("column_value", "Enter value for column:"),
+#       actionButton("submit", "Submit")
+#     ),
+#     mainPanel(
+#       p("Enter the evalue you want to query."),
+#       # Add table or other visualizations to display the subset of the table
+#       tableOutput("table")
+#     )
+#   )
+# )
+# 
+# # Define server
+# server <- function(input, output) {
+#   # Read CSV file from GitHub repository
+#   data <- read.csv("https://raw.githubusercontent.com/sr320/course-fish546-2023/main/output/blast_annot_go.tab", sep = '\t', header = TRUE)
+#   
+#   # Define reactive expression to subset table based on user input
+#  subset_data <- reactive({
+#    subset(data, V13 == input$column_value) # Replace COLUMN_NAME with the name of the column you want to filter on
+#  })
+#   
+# 
+#   
+#   # Render table based on subset_data reactive expression
+#   output$table <- renderTable({
+#     subset_data()
+#   })
+# }
+# 
+# # Run the app
+# shinyApp(ui, server)
