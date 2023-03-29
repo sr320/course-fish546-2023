ce4b092a182f96cb7e9b897559e18db3f5c865bb
Steven Roberts <sr320@uw.edu>
2023-03-29 09:46:19 -0700

    shiny app test

--- myappp/app.R
+++ myappp/app.R
@@ -1,51 +1,36 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(dplyr)
 
-# Define UI for application that draws a histogram
+# Define UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  sidebarLayout(
+    sidebarPanel(
+      # Add input field for the user to provide a value for the column
+      textInput("column_value", "Enter value for column:"),
+      actionButton("submit", "Submit")
+    ),
+    mainPanel(
+      # Add table or other visualizations to display the subset of the table
+      tableOutput("table")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+# Define server
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  # Read CSV file from GitHub repository
+  data <- read.csv("../output/blast_annot_go.tab")
+  
+  # Define reactive expression to subset table based on user input
+  subset_data <- reactive({
+    subset(data, Gene.Ontology..biological.process == input$column_value) # Replace COLUMN_NAME with the name of the column you want to filter on
+  })
+  
+  # Render table based on subset_data reactive expression
+  output$table <- renderTable({
+    subset_data()
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Run the app
+shinyApp(ui, server)
