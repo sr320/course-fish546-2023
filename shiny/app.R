24fd371312bb226f63b82bebfc854ca562e701fc
Steven Roberts <sr320@uw.edu>
2023-03-29 10:08:08 -0700

    shiny

--- shiny/app.R
+++ shiny/app.R
@@ -19,11 +19,11 @@ ui <- fluidPage(
 # Define server
 server <- function(input, output) {
   # Read CSV file from GitHub repository
-  data <- read.csv("../output/blast_annot_go.tab")
+  data <- read.csv("../output/blast_annot_go.tab", sep = '\t', header = TRUE)
   
   # Define reactive expression to subset table based on user input
   subset_data <- reactive({
-    subset(data, Gene.Ontology..biological.process == input$column_value) # Replace COLUMN_NAME with the name of the column you want to filter on
+    subset(data, V3 == input$column_value) # Replace COLUMN_NAME with the name of the column you want to filter on
   })
   
   # Render table based on subset_data reactive expression
